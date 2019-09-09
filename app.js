29b4927085d61335c6913e1ef255703344d3698d
buiminhhai1 <ngovietduc20088@gmail.com>
2019-09-09 10:38:19 +0700

    basic router and interact with models

---
+++ app.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const notFound = require('./controllers/404');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData);
+app.use(shopRoutes);
+
+app.use(notFound.notFound);
+
+app.listen(3000);
